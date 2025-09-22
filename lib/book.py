913726f2a25953dfe6bf5ca376d6685744801206
Ann Gathoni <theanngathoni@gmail.com>
2025-09-22 14:49:27 +0300

    object oriented programming

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # property for page_count with validation
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # method required by test
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     pass
     
         
